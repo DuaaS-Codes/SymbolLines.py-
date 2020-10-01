45179f99437eeac8e5a51b7299a9c73b85067878
Duaa S <72174360+DuaaS-Codes@users.noreply.github.com>
2020-09-30 22:32:33 -0400

    Create SymbolLines.py

---
+++ SymbolLines.py
@@ -0,0 +1,10 @@
+#Author: Duaa 
+#Date: December 6, 2019
+#Q4
+
+int_length = (int)(input("Enter a length: ")) #Prompts user to enter length
+str_character = (input("Enter a character: ")) #Prompts user to enter a character
+
+for i in range(1): #Prints line of code 1 time
+    print(str_character * int_length) #Print statement used 
+                                      #To multiply str_character by int_length 
